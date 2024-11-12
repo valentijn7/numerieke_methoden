f749fc1afb45911d589c19b13f7ff78c2d5a8e4c
valentijn7 <v.w.oldenburg@student.rug.nl>
2024-11-12 19:27:56 +0100

    Finished the code of exercise I

--- 1_fractal/fractal.py
+++ 1_fractal/fractal.py
@@ -1,7 +1,16 @@
+# 1_fractal/fractal.py
+
+import sys
 from typing import List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
+#! Vraag hoe het zit met de complexe getallen in de opdracht, en ook
+#! hoe het zit met afronden enzo
+# Manual: to run the various parts of the code, use the following command:
+# python fractal.py <opdracht>
+# where <opdracht> is an integer between 1 and 5, corresponding to the exercise
 
 ############# PART 1 ##############
 """
@@ -122,60 +131,121 @@ x0 = +2, +10, 0.5, 0
 To what root does each guess converge?
 """
 
-def symmetric_five_point_formula(x: float, delta_x: float, f: ) -> float:
-    """
-    Calculates (15) in the lecture notes, the symmetric five-point formula
 
-    :param x: the point at which to evaluate the derivative
-    :param delta_x: the step size
-    :param f: the function to differentiate
-    :return: the derivative of f at x
+def df(x: float, roots: List[float], dx: float = 1e-6) -> float:
     """
-
-def df(x: float, roots: List[float]) -> float:
-    """
-    df: polynomial derivative
-    x: evaluate f' at x
-    roots: numpy array of roots of the polynomial
-
     We use numerical differentiation to calculate the derivative at x
     of a polynomial f(x) defined
 
     f(x) = (r1 - x) * (r2 - x) * ... * (rn - x)
 
-    where r1, r2, ..., rn are the roots of the polynomial (see (59)
+    where r1, r2, ..., rn are the roots of the polynomial (see Eq. (59)
     in the lecture notes). To obtain f'(x), we differentiate with
-    the 'symmetric five-point formula', (15) in the lecture notes.
+    the 'symmetric three-point formula', Eq. (13) in the lecture notes.
+    (Only the first term on the right-hand side of Eq. (13) is used here.)
 
-    Returns the value of f'(x).
+    :param x : the point at which to evaluate the derivative
+    :param roots : the roots of the polynomial
+    :param dx : the step size for differentiation
+    :return : the (polynomial) derivative of f at x
+    """
+    return (f(x + dx, roots) - f(x - dx, roots)) / (2 * dx)
+
+
+def execute_part_2():
+    """
+    Executes ex. 1.2, by checking a few deritatives at different points for
+    a general polynomial (as defined in Eq. (59) in the lecture notes).
+    """
+    roots = [1, -1]
+    x_values = [0, 1, 2, 3]
+    for x in x_values:
+        print(f"f'({x}) = approx. {df(x, roots)}")
+
+    ### Outputs:
+    """
+    f'(0) = approx. 0.0
+    f'(1) = approx. 1.999999999946489
+    f'(2) = approx. 4.000000000337067
+    f'(3) = approx. 6.000000000838668
     """
 
-    
 
-
-
-
-def newton_method(roots, x0, maxiter=1000):
+def round_guess(x: float, error: float, n: int, dec: int = 6) -> Tuple[float, float, int]:
     """
-    roots: list of polynomial roots
-    x0: double with the initial guess
-    maxiter: maximum iterations to prevent infinite loops
-    
-    Function that determines the root of the equation f(x) = 0 within the interval (a, b) using the Brent's method.
+    Helper function to newton_raphson_method(); it rounds the result to
+    the desired precision, adjust approximated but unprecise imaginary
+    parts, and returns the result as a tuple (Root, Error, Number of Iterations)
 
-    Returns a tuple (Root, Error, Number of Iterations)
+    :param x : the root found by the Newton-Raphson method
+    :param error : the error in the root
+    :param n : the number of iterations
+    :param dec : the number of decimals to round the result to
     """
-    #YOUR CODE GOES HERE
+    # force imaginary part to be zero if it is very small
+    if isinstance(x, complex) and abs(x.imag) < 1.0e-6:
+        x = x.real
+    return np.round(x, dec), np.round(error, dec), n
 
+
+
+def newton_raphson_method(
+        roots: List[float], x0: float, maxiter: int = 1000, dec: float = 6
+    ) -> Tuple[float, float, int]:
+    """
+    Function that determines the root of the equation f(x) = 0 within
+    the interval (a, b) using the Newton-Raphson method.
+
+    :param roots : list of polynomial roots
+    :param x0 : double with the initial guess
+    :param maxiter : maximum iterations (to prevent infinite loops)
+    :param dec : number of decimals to round the result to
+    :return : a tuple (Root, Error, Number of Iterations)
+    """
+    n = 0
+    tolerance = 1.0e-6
+
+    while n < maxiter:
+        fx = f(x0, roots)       # calculate f(x0)
+        dfx = df(x0, roots)     # calculate f'(x0)
+        if abs(dfx) < 1.0e8:    # if f'(x0) is smaller than 1.0e8, perturb it slightly
+                                # into the complex plane to prevent division by zero
+            dfx += (1.0e-6 + 1.0e-6j)
+        x1 = x0 - fx / dfx      # calculate x1 (as per Eq. (2))
+                                # if within the error tolerance, return result
+        if abs(x1 - x0) < tolerance:
+            return round_guess(x1, abs(x1 - x0), n, dec)
+        
+        x0 = x1                 # update x0 and n
+        n += 1
+                                # if the loop exits, the method did not converge
+    # print(f'newton_raphson_method(): did not converge after {n} iterations')
+    return x1, abs(x1 - x0), n
+#! For the report, keep in mind the story about the Real and Imaginary parts, and how
+#! to choose the thresholds/precision etc., besides all the normal stuff to tell
 
 def execute_part_3():
-    #Try to find a solution using the Newton's method
-    #YOUR CODE GOES HERE
+    """
+    Try to find a solution using the Newton's method for Eq. (60),
+    which equivalently reads: f(x) = (x + 1) * (x - 1). To which
+    root does each initial guess converge?
+    """
+    roots = [-1, 1] 
+    initial_guesses = [-2, 10, 0.5, 0]
 
-    roots = [...] # array with list of roots
+    print('\nRoot, Error, Iterations')
+    for x0 in initial_guesses:
+        print(newton_raphson_method(roots, x0))
+    print()
 
-    x0 = 1
-    newton_method(roots, x0)
+    ### Outputs:
+    """
+    Root, Error, Iterations
+    (-1.0, 0.0, 4)
+    (1.0, 0.0, 7)
+    (1.0, 0.0, 4)
+    (1.0, 0.0, 24)
+    """
 
 
 ############# PART 4 ##############
@@ -192,14 +262,26 @@ To what root each guess converges? Does it always return a correct root?
 """
 
 def execute_part_4():
-    #Try to find a solution using the Newton's method
-    #YOUR CODE GOES HERE
+    """
+    Executes ex. 1.4, by checking the convergence of the Newton's method
+    for a complex polynomial, using the initial guesses from above
+    """
+    roots = [1 + 1j, 1 - 1j]
+    initial_guesses = [2 + 1j, -1j, 3, 2]
 
-    roots = [...] # array with list of complex roots
+    print('\nRoot, Error, Iterations')
+    for x0 in initial_guesses:
+        print(newton_raphson_method(roots, x0))
+    print()
 
-    #Test your Newton's code with complex numbers
-    x0 = 1+1j
-    newton_method(roots, x0)
+    ### Outputs:
+    """
+    Root, Error, Iterations
+    ((1+1j), 0.0, 5)
+    ((1-1j), 0.0, 5)
+    ((1+1j), 1e-06, 24)
+    ((1+1j), 0.0, 24)
+    """
 
 ############# PART 5 ##############
 """
@@ -222,35 +304,78 @@ Notice: to save computing time we will only consider maximum 4 Newton iterations
 """
 
 def execute_part_5():
-    #Try to obtain the Newton's fractal
-    #YOUR CODE GOES HERE
-
-    roots = [...] # array with list of complex roots
+    """
+    Executes ex. 1.5, by plotting the Newton's fractal for the polynomial
+    f(z) = (1 + 1j - z) * (+1 - 1j - z) * (-1 + 1j - z) * (-1 - 1j - z),
+    using gerenal initial guesses
+    """
+    roots = [1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j]
 
     x_min, x_max, nx = -2, +2, 10 # minimum, maximum value of x, and number of x points
     y_min, y_max, ny = -2, +2, 10 # minimum, maximum value of y, and number of y points
 
+    maxiter = 4
     Z = np.empty((nx, ny))
 
-    for i, x in enumerate(np.linspace(x_min, x_max, nx)):
-        for j, y in enumerate(np.linspace(x_min, x_max, nx)):
-            root = newton_method(roots, x0, 4)
+    for idx, x in enumerate(np.linspace(x_min, x_max, nx)):
+        for jdx, y in enumerate(np.linspace(x_min, x_max, nx)):
+            # get initial guess and calculate the root with ... iterations
+            x0 = x + y * 1j
+            root, _, _ = newton_raphson_method(roots, x0, maxiter)
 
-            # Z[j, i] = #YOUR CODE TO FIND THE CLOSEST ROOT GOES HERE
-
+            # find the closest root, or, in other words, the root with
+            # the smallest absolute difference to the calculated root
+            closest_root = np.argmin([abs(root - r) for r in roots])
+            Z[jdx, idx] = closest_root
 
     # plot script
     fig, ax1 = plt.subplots()
 
-    plot = ax1.imshow(Z, extent=(x_min, x_max, y_min, y_max), vmin=0, vmax=len(roots)-1, interpolation='none')
-    fig.colorbar(plot, ax=ax1, ticks=np.arange(len(roots)))
-    ax1.scatter(roots.real, roots.imag, color='k', marker='X', edgecolors='w')
-
+    plot = ax1.imshow(Z,
+                      extent = (x_min, x_max, y_min, y_max),
+                      vmin = 0,
+                      vmax = len(roots) - 1,
+                      interpolation = 'none')
+    fig.colorbar(plot,
+                 ax = ax1,
+                 ticks = np.arange(len(roots)))
+    ax1.scatter([r.real for r in roots],
+                [r.imag for r in roots],
+                color = 'k',
+                marker = 'X',
+                edgecolors = 'w')
+    plt.savefig(f'newton_fractal_{maxiter}_iterations.pdf')
     plt.show()
     
 
+def validate_args(args: List[str]) -> bool:
+    """
+    Does a quick check on whether the argument (a number between 1 and 5)
+    is correctly and entered and thus valid. Else, it raises an error.
+    
+    :param args : a list of command line arguments
+    :return : True if the argument is valid, False otherwise
+    """
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) not in range(1, 6):
+        raise ValueError("Usage: python fractal.py <opdracht>"\
+                         "\n<opdracht> should be an integer between 1 and 5")
+    return True
+    
+
 def main():
-    execute_part_1()
+    if validate_args(sys.argv):
+        part = int(sys.argv[1])
+    
+    if part == 1:
+        execute_part_1()
+    elif part == 2:
+        execute_part_2()
+    elif part == 3:
+        execute_part_3()
+    elif part == 4:
+        execute_part_4()
+    elif part == 5:
+        execute_part_5()
     
 
 if __name__ == '__main__':
