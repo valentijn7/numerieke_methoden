492e9aea924260cd01c98894007e9f0eaf6b587b
valentijn7 <v.w.oldenburg@student.rug.nl>
2025-01-09 19:59:35 +0100

    Added Leap-Frog, Adams-Bashforth

--- III/task1.py
+++ III/task1.py
@@ -1,8 +1,5 @@
 # numerieke_methoden/III/Task1_MandatoryStructure.py
 
-"""
-@author: Maarten van der Molen & Valentijn Oldenburg
-"""
 from typing import List, Dict, Tuple, Any, Callable
 from scipy import special
 import numpy as np
@@ -17,13 +14,13 @@ class PhysConstants:
         self.T0      = 273      # initial temperature rod (K)
         self.T1      = 373      # temperature of rod at x = 0 for t > 0
                                 #   rod properties:
-        self.L       = 10       # length of the rod (m)
-        self.n_x     = 100       # number of gridpoints in the rod
+        self.L       = 10        # length of the rod (m)
+        self.n_x     = 100      # number of gridpoints in the rod
                                 # step size of the grid
         self.dx      = self.L / self.n_x
                                 #   simulation properties:
-        self.t_total = 0.05     # total time of the simulation (s)
-        self.dt      = 0.001        # time step of the simulation (s)
+        self.t_total = 0.01     # total time of the simulation (s)
+        self.dt      = 0.0001   # time step of the simulation (s)
                                 # number of time steps
         self.n_t     = int(self.t_total / self.dt)
 
@@ -33,7 +30,7 @@ def forward_euler(
         current_time: float,
         step_size: float,
         derivative: callable,
-        d_c: Dict[str, float]
+        C: PhysConstants
     ) -> np.array:
     """ Performs one step of the Forward Euler method
     
@@ -41,11 +38,54 @@ def forward_euler(
     :param current_time: current time of the system (independent variable)
     :param step_size: step size of the integration
     :param derivative: function that returns derivatives for current state and time
-    :param d_c: dictionary with constants
+    :param C: class (struct) with constants
     :return: updated values of dependent variable at next time step
     """
-    slope = derivative(current_state, current_time, d_c)
-    return current_state + step_size * slope
+    return current_state + step_size * derivative(current_state, current_time, C)
+
+
+def leap_frog(
+        prev_state: np.array,
+        current_state: np.array,
+        current_time: float,
+        step_size: float,
+        derivative: callable,
+        C: PhysConstants
+    ) -> np.array:
+    """ Performs one step of the Leap Frog method
+
+    :param prev_state: previous state of the system (dependent variable)
+    :param current_state: current state of the system (dependent variable)
+    :param current_time: current time of the system (independent variable)
+    :param step_size: step size of the integration
+    :param derivative: function that returns derivatives for current state and time
+    :param C: class (struct) with constants
+    :return: updated values of dependent variable at next time step
+    """
+    return prev_state + 2 * step_size * derivative(current_state, current_time, C)
+
+
+def adam_bashforth(
+        prev_state: np.array,
+        current_state: np.array,
+        current_time: float,
+        step_size: float,
+        derivative: callable,
+        C: PhysConstants) -> np.array:
+    """
+    Performs one step of the Adams-Bashforth method
+
+    :param prev_state: previous state of the system (dependent variable)
+    :param current_state: current state of the system (dependent variable)
+    :param current_time: current time of the system (independent variable)
+    :param step_size: step size of the integration
+    :param derivative: function that returns derivatives for current state and time
+    :param C: class (struct) with constants
+    :return: updated values of dependent variable at next time step
+    """
+    return current_state + step_size * \
+        (1.5 * derivative(current_state, current_time, C) \
+         - 0.5 * derivative(prev_state, current_time, C))
 
 
 def RK4(
@@ -69,7 +109,6 @@ def RK4(
     k2 = step_size * derivative(current_state + 0.5 * k1, current_time + 0.5 * step_size, constants)
     k3 = step_size * derivative(current_state + 0.5 * k2, current_time + 0.5 * step_size, constants)
     k4 = step_size * derivative(current_state + k3, current_time + step_size, constants)
-
                             # calculate and return new state
     return current_state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
@@ -77,7 +116,7 @@ def RK4(
 def derivative_heat(
         T: np.array,
         _: float,
-        cons: PhysConstants) -> float:
+        cons: PhysConstants) -> np.array:
     """ Calculates the derivative of the rod using the heat equation
     
     :param T: temperature array of the rod
@@ -93,22 +132,26 @@ def derivative_heat(
     for idx in range(1, len(T) - 1):
         T_hat[idx] = (T[idx + 1] - 2.0 * T[idx] + T[idx - 1]) / (cons.dx**2)
 
-    return T_hat
+    return cons.kappa * T_hat
 
 
-def double_derivative_heat(
-        T: np.array,
-        _: float,
-        cons: PhysConstants
-    ) -> float:
-    """ Wrapper function for double derivative
+def theoretical(
+        x_arr: np.array,
+        t: int,
+        C: PhysConstants
+    ) -> np.array:
+    """ Theoretical solution of the heat equation;
+    the erfc function is imported from the scipy library:
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.erfc.html
 
-    :param T: temperature array of the rod
-    :param _: we discard the time parameter
-    :param cons: class (struct) with constants
-    :return: second derivative of the array
+    :param x_arr: array with grid positions
+    :param t: current time
+    :return: array with temperatures at grid positions
     """
-    return cons.kappa * derivative_heat(T, _, cons)
+    if t == 0:
+        return np.full_like(x_arr, C.T0)
+    else:
+        return C.T0 + (C.T1 - C.T0) * special.erfc(x_arr / (2 * np.sqrt(C.kappa * t)))
 
 
 def Task1_caller(L: int,
@@ -154,95 +197,131 @@ def Task1_caller(L: int,
     time = np.arange(0, t_total, dt)
     space = np.arange(0, L, L / nx)
                                 # third, solve heat equation
-    for idx in range(1, len(time)):
-        grid[:, idx] = RK4(grid[:, idx - 1], time[idx - 1], dt, double_derivative_heat, C)
+    if TimeSteppingMethod == "Theory":
+        for idx in range(1, len(time)):
+            grid[:, idx] = theoretical(space, time[idx], C)
+
+    elif TimeSteppingMethod == "FE":
+        for idx in range(1, len(time)):
+            grid[:, idx] = forward_euler(grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+
+    elif TimeSteppingMethod == "LF":
+        for idx in range(1, len(time)):
+            if idx == 1:
+                grid[:, idx] = forward_euler(grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+            else:
+                grid[:, idx] = leap_frog(grid[:, idx - 2], grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+
+    elif TimeSteppingMethod == "AB":
+        for idx in range(1, len(time)):
+            if idx == 1:
+                grid[:, idx] = forward_euler(grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+            else:
+                grid[:, idx] = adam_bashforth(grid[:, idx - 2], grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+
+    elif TimeSteppingMethod == "RK4":
+        for idx in range(1, len(time)):
+            grid[:, idx] = RK4(grid[:, idx - 1], time[idx - 1], dt, derivative_heat, C)
+
+    else:
+        raise ValueError("Invalid TimeSteppingMethod")
 
     return time, space, grid   
 
 
-def theoretical(
-        x_arr: np.array,
-        t: int,
-        C: PhysConstants
-    ) -> np.array:
-    """ Theoretical solution of the heat equation.
-    The erfc function is imported from the scipy library:
-    https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.erfc.html
-
-    :param x_arr: array with grid positions
-    :param t: current time
-    :return: array with temperatures at grid positions
-    """
-    if t == 0:
-        return np.full_like(x_arr, C.T0)
-    else:
-        return C.T0 + (C.T1 - C.T0) * special.erfc(x_arr / (2 * np.sqrt(C.kappa * t)))
-
-
-def analyse_results(
-        time: np.array,
-        space: np.array,
-        grid: np.array
-    ) -> None:
-    """ Analyse the results with e.g. plots
-
-    :param time: time array
-    :param space: space array
-    :param grid: grid with results
-    """
-    fig, ax = plt.subplots()    # unpack Tuple with , for single element
-    line_th, = ax.plot([], [], lw = 2, color = 'blue')
-    line_num, = ax.plot([], [], lw = 2, color = 'red')
-
-                                # print the grid for debugging purposes
-    for idx in range(len(grid[:, ])):
-        print(grid[idx, :])
-                                # set limits, labels, grid, legend, ticks
-    ax.set_xlim(space[0], space[-1])
-    ax.set_ylim(np.nanmin(grid), np.nanmin(grid))
-    ax.set_xlabel("rod (m)", fontsize = 16)
-    ax.set_ylabel("temperature (K)", fontsize = 16)
-    ax.grid(True)
-    ax.legend(["Theoretical", "Numerical"], fontsize = 16)
-    plt.xticks(fontsize = 14)
-    plt.yticks(fontsize = 14)
-
-    def init():                 # create empty plot animation
-        line_num.set_data([], [])
-        line_th.set_data([], [])
-        return line_num, line_th
-
-    def update(frame):          # update plot animation with new data
-        y_num = grid[:, frame]  # numerical solution
-        line_num.set_data(space, y_num)
-                                # theoretical solution
-        y_th = theoretical(space, time[frame], PhysConstants())
-        line_th.set_data(space, y_th)
-
-        ax.set_title(f"t = {time[frame]:.2f} s", fontsize = 16)
-        return line_num, line_th
-
-    # https://matplotlib.org/stable/api/_as_gen/matplotlib.animation.FuncAnimation.html
-    ani = animation.FuncAnimation(fig,
-                                  update ,
-                                  frames = len(time),
-                                  init_func = init,
-                                  interval = 100)
-
-    plt.show()
-
-
 def main():
+    #! TODO bereken die ratio van de lecture slides
     C = PhysConstants()
 
-    time, space, grid = Task1_caller(
+    time, space, grid_th = Task1_caller(
+        C.L,
+        C.n_x,
+        C.t_total,
+        C.dt,
+        "Theory")
+
+    time, space, grid_FE = Task1_caller(
+        C.L,
+        C.n_x,
+        C.t_total,
+        C.dt,
+        "FE")
+    
+    time, space, grid_LF = Task1_caller(
+        C.L,
+        C.n_x,
+        C.t_total,
+        C.dt,
+        "LF")
+    
+    time, space, grid_AB = Task1_caller(
+        C.L,
+        C.n_x,
+        C.t_total,
+        C.dt,
+        "AB")
+    
+    time, space, grid_RK4 = Task1_caller(
         C.L,
         C.n_x,
         C.t_total,
         C.dt,
         "RK4")
     
-    analyse_results(time, space, grid)
+    ######### PLotting #########
+
+    grids = [grid_th, grid_FE, grid_LF, grid_AB, grid_RK4]
+
+    fig, ax = plt.subplots()    # unpack Tuple with , for single element
+    line_th, = ax.plot([], [], lw = 2, label = "theoretical")
+    line_FE, = ax.plot([], [], lw = 2, label = "forward Euler")
+    line_LF, = ax.plot([], [], lw = 2, label = "leap-frog")
+    line_AB, = ax.plot([], [], lw = 2, label = "Adams-Bashforth")
+    line_RK4, = ax.plot([], [], lw = 2, label = "Runge-Kutta 4")
+
+                                # set limits, labels, grid, legend, ticks
+    ax.set_xlim(space[0], space[-1])
+    ax.set_ylim(np.nanmin(grids), np.nanmax(grids))
+    ax.set_xlabel("rod (m)", fontsize = 16)
+    ax.set_ylabel("temperature (K)", fontsize = 16)
+    ax.grid(True)
+    plt.legend(fontsize = 16)
+    plt.xticks(fontsize = 16)
+    plt.yticks(fontsize = 16)
+
+    def init():                 # create empty plot animation
+        line_FE.set_data([], [])
+        line_LF.set_data([], [])
+        line_AB.set_data([], [])
+        line_RK4.set_data([], [])
+        line_th.set_data([], [])
+        return line_FE, line_LF, line_AB, line_RK4, line_th
+
+    def update(frame):          # update plot animation with new data
+                                # theoretical solution
+        y_th = grid_th[:, frame]
+        line_th.set_data(space, y_th)
+                                # numerical solutions
+        y_FE = grid_FE[:, frame]
+        line_FE.set_data(space, y_FE)
+        y_LF = grid_LF[:, frame]
+        line_LF.set_data(space, y_LF)
+        y_AB = grid_AB[:, frame]
+        line_AB.set_data(space, y_AB)
+        y_RK4 = grid_RK4[:, frame]
+        line_RK4.set_data(space, y_RK4)
+
+        ax.set_title(f"t = {time[frame]:.4f} s", fontsize = 16)
+        return line_FE, line_LF, line_AB, line_RK4, line_th
+
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.animation.FuncAnimation.html
+    ani = animation.FuncAnimation(fig,
+                                  update,
+                                  frames = len(time),
+                                  init_func = init,
+                                  interval = 100)
+
+    plt.show()
 
 
 if __name__ == "__main__":
