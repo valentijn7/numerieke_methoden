81f72d89b2d40d14438723ce64e6234974124255
valentijn7 <v.w.oldenburg@student.rug.nl>
2025-01-23 17:15:19 +0100

    Enge wateranimatie

--- III/Navier-Stokes/algorithms.py
+++ III/Navier-Stokes/algorithms.py
@@ -3,206 +3,15 @@
 from typing import List, Dict, Tuple, Any, Callable
 import numpy as np
 
-from typing import List, Dict, Tuple, Any, Callable
-import numpy as np
 
-
-'''    
-def forward_euler(
-        current_height: np.ndarray,
-        current_velocity: np.ndarray,
-        current_b: np.ndarray,
-        dt: float,
-        derivative: callable,
-        C: Any
-    ) -> Tuple[np.ndarray, np.ndarray]:
-    """ Performs one step of the Forward Euler method
-    # TODO! misschien toch nog terugveranderen !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    :param current_state: current state of the system 
-    :param current_velocity: current velocity of the system 
-    :param current_b : current rest height of water
-    :param dt: step size of time iteration
-    :param derivative: function that returns derivatives for current state and time
-    :param C: class (struct) with constants
-    :return: updated values of dependent variable at next time step
-    """
-    dH_dt = derivative(current_height, current_velocity,current_b,C.dt,C.dx,C)[0]
-    du_dt = derivative(current_height, current_velocity,current_b,C.dt,C.dx,C)[1]
-    
-    H_new = current_height + dt * dH_dt
-    u_new = current_velocity + dt * du_dt
-    
-    return (H_new, u_new)
-'''
-
-def forward_euler(# 8 arguments
-        current_state: np.ndarray,
-        current_height: np.ndarray,
-        current_velocity: np.ndarray,
-        current_b: np.ndarray,
-        dt: float,
-        dx: float,
-        derivative: callable,
-        C: Any
-    ) -> np.array:
-    """ Performs one step of the Forward Euler method
-    :param current_state: current state of the system (either height or velocity)
-    :param current_height: current height of the system
-    :param current_velocity: current velocity of the system 
-    :param current_b : current rest height of water
-    :param dt: step size of time iteration
-    :param dx: step size of space iteration
-    :param derivative: function that returns derivatives for current state and time
-    :param C: class (struct) with constants
-    :return: updated values of dependent variable at next time step
-    """
-    new_state = current_state + dt * derivative(
-        current_height,
-        current_velocity,
-        current_b,
-        dt,
-        dx,
-        C
-    )
-    
-    return new_state
-
-
-def leap_frog(# 9 arguments
-        prev_state: np.array,
-        current_state: np.ndarray,
-        current_height: np.ndarray,
-        current_velocity: np.ndarray,
-        current_b: np.ndarray,
-        dt: float,
-        dx: float,
-        derivative: callable,
-        C: Any
-    ) -> np.array:
-    """ Performs one step of the Leap Frog method
-
-    :param prev_state: previous state of the system (either height or velocity)
-    :param current_state: current state of the system (either height or velocity)
-    :param current_height: current height of the system
-    :param current_velocity: current velocity of the system 
-    :param current_b : current rest height of water
-    :param dt: step size of time iteration
-    :param dx: step size of space iteration
-    :param derivative: function that returns derivatives for current state and time
-    :param C: class (struct) with constants
-    :return: updated values of dependent variable at next time step
-    
-    """
-    new_state = prev_state + 2 * dt * derivative(current_height,
-                                              current_velocity,
-                                              current_b,
-                                              dt, dx, C)
-
-    return new_state
-
-
-def adam_bashforth(# 10 arguments
-        current_state: np.ndarray,
-        prev_height: np.ndarray,
-        prev_velocity: np.ndarray,
-        current_height: np.ndarray,
-        current_velocity: np.ndarray,
-        current_b: np.ndarray,
-        dt: float,
-        dx: float,
-        derivative: callable,
-        C: Any
-    ) -> np.array:
-    """
-    Performs one step of the Adams-Bashforth method
-    
-    :param prev_height: height of system for the previous timestep
-    :param prev_height: velocity of system for the previous timestep
-    :param current_state: current state of the system (either height or velocity)
-    :param current_height: current height of the system
-    :param current_velocity: current velocity of the system 
-    :param current_b : current rest height of water
-    :param dt: step size of time iteration
-    :param dx: step size of space iteration
-    :param derivative: function that returns derivatives for current state and time
-    :param C: class (struct) with constants
-    :return: updated values of dependent variable at next time step
-    """
-    
-    return current_state + dt * \
-        (1.5 * derivative(current_height, current_velocity,
-                                                  current_b,
-                                                  dt, dx, C) \
-         - 0.5 * derivative(prev_height, prev_velocity,
-                                                   current_b,
-                                                   dt, dx, C))
-
-
-def init_crank_nicolson(
-        C: Any
-) -> Tuple[np.array, np.array]:
-    """
-    Init, precompute, and return the matrices for the Crank-nicolson 
-
-    Assumption: Dirichlet boundary conditions
-
-    :param C: class (struct) with constants
-    :return: matrices A and B
-    """
-    n = C.n_x + 1
-    A = np.zeros((n, n), dtype = float)
-    B = np.zeros((n, n), dtype = float)
-
-    sigma = C.kappa * C.dt / (C.dx ** 2)
-                            # init main- and off-diagonal elements, rest is zero
-    for idx in range(1, n - 1):
-        A[idx, idx] = 1 + sigma
-        A[idx, idx - 1] = -sigma / 2
-        A[idx, idx + 1] = -sigma / 2
-
-        B[idx, idx] = 1 - sigma
-        B[idx, idx - 1] = sigma / 2
-        B[idx, idx + 1] = sigma / 2
-
-                            # Dirichlet boundary conditions:
-    A[0, :] = 0             # zero out first row;
-    A[0, 0] = 1             # add 1 on diagonal
-    B[0, :] = 0
-    B[0, 0] = 1
-
-    A[-1, :] = 0            # same for final row
-    A[-1, -1] = 1
-    B[-1, :] = 0
-    B[-1, -1] = 1
-
-    return A, B
-
-
-def crank_nicolson(
-        current_state: np.array,
-        A: np.array,
-        B: np.array,
-        constants: Any
-) -> np.array:
-    """
-    Performs one step of the Crank-nicolson method (with Dirichlet)
-
-    :param current_state: current state of the system (dependent variable)
-    :param A: matrix A
-    :param B: matrix B
-    :param constants: class (struct) with constants
-    :return: update to next time step
-    """
-    intermediate = np.dot(B, current_state)
-    intermediate[0], intermediate[-1] = constants.T1, constants.T0
-    return np.linalg.solve(A, intermediate)
+# For the SWE, we only use Runge-Kutta 4th order.
 
     
-def runge_kutta_4(# 8 arguments
-        current_state: np.ndarray,
-        current_height: np.ndarray,
-        current_velocity: np.ndarray,
-        current_b: np.ndarray,
+def RK4(
+        state: np.ndarray,
+        h: np.ndarray,
+        v: np.ndarray,
+        b: np.ndarray,
         dt: float,
         dx: float,
         derivative: callable,
@@ -210,10 +19,10 @@ def runge_kutta_4(# 8 arguments
     ) -> np.array:
     """ Performs one step of the 4th order Runge-Kutta
     
-    :param current_state: current state of the system (either height or velocity)
-    :param current_height: current height of the system
-    :param current_velocity: current velocity of the system 
-    :param current_b : current rest height of water
+    :param state: current state of the system (either height or velocity)
+    :param h: current height of the system
+    :param v: current velocity of the system 
+    :param b : current rest height of water
     :param dt: step size of time iteration
     :param dx: step size of space iteration
     :param derivative: function that returns derivatives for current state and time
@@ -221,9 +30,9 @@ def runge_kutta_4(# 8 arguments
     :return: updated values of dependent variable at next time step
     """
                             # calculate the 4 k's
-    k1 = dt * derivative(current_height,current_velocity, current_b, dt, dx, C)
-    k2 = dt * derivative(current_height + 0.5 * dt * k1, current_velocity + 0.5 * dt * k1, current_b, dt, dx, C)
-    k3 = dt * derivative(current_height + 0.5 * dt * k2, current_velocity + 0.5 * dt * k2, current_b, dt, dx, C)
-    k4 = dt * derivative(current_height + dt    *    k3, current_velocity + dt    *    k3, current_b, dt, dx, C)
+    k1 = dt * derivative(h, v, b, dt, dx, C)
+    k2 = dt * derivative(h + 0.5 * k1, v + 0.5 * dt * k1, b, dt, dx, C)
+    k3 = dt * derivative(h + 0.5 * k2, v + 0.5 * dt * k2, b, dt, dx, C)
+    k4 = dt * derivative(h + k3, v + k3, b, dt, dx, C)
                             # calculate and return new state
-    return current_state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    return state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
--- III/Navier-Stokes/task3.py
+++ III/Navier-Stokes/task3.py
@@ -1,18 +1,11 @@
 # numerieke_methoden/III/Navier-Stokes/task3.py
 
 from typing import List, Dict, Tuple, Any, Callable
-from scipy import special
-from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from algorithms import forward_euler
-from algorithms import leap_frog
-from algorithms import adam_bashforth
-from algorithms import init_crank_nicolson
-from algorithms import crank_nicolson
-from algorithms import runge_kutta_4
+from algorithms import RK4
 
 
 # Note that every time the term "velocity" is used in this script,
@@ -20,7 +13,8 @@ from algorithms import runge_kutta_4
 
 
 class PhysConstants:
-    def __init__(self):
+    def __init__(self):         
+                                #   constants:
         self.g       = 9.81     # gravitational acceleration
         self.b       = 1        # height of water at rest (constant)
         self.beta    = 0.01     # the constant parametrising friction
@@ -38,16 +32,16 @@ class PhysConstants:
         self.dt      = self.t_total / self.n_t
 
 
-def central_derivative(
+def CD(
         state: np.ndarray, 
         step_size: float
     ) -> np.array:
     """
-    Uses central differentiation to differentiate a given input array.
-    At the ends, forward and backward differentiation are used.
+    Uses central differentiation to differentiate a given input array;
+    at the ends, forward and backward differentiation are used.
     
     :param state: input array of the variable
-    :param step_size: size of the step over which is differentiated.
+    :param step_size: size of the step over which is differentiated
     :return: differentiated array
     """
     central_diff = np.zeros(len(state))
@@ -82,39 +76,8 @@ def H_time_deriv(
                         # second: use CD to get the spacial derivative of H*u,
                         #         and compute dH_dt
     H_u = height * velocity
-    dH_dt = -central_derivative(H_u, dx)
+    dH_dt = -CD(H_u, dx)
     return dH_dt
-
-
-# def u_time_deriv(height: np.ndarray, 
-#                  velocity: np.ndarray,
-#                  b: np.ndarray,
-#                  dt: float,
-#                  dx: float,
-#                  C: Any
-#                  ) -> np.ndarray:
-#     """ 
-#     Function that generates values for the time derivative of the velocity
-#     for one iteration step over dt
-    
-#     :param height: array containing the heights of the water at point t
-#     :param velocity: array containing the (horizontal) velocities of the water at point t
-#     :param b: array of water heights at rest
-#     :param dt: temporal step size
-#     :param dx: spacial step size
-#     :param C: class containing physical constants
-#     :return: array containing the time derivative of the velocity
-#     """
-#                         # first:  calculate dH_dt;
-#                         # second: create an array for H*u^2;
-#                         # third:  use CD to get the spacial derivative of H*u^2;
-#                         # fourth: calculate the third term of the equation;
-#                         # fifth:  calculate the time derivative of the velocity
-#     dH_dt = H_time_deriv(height, velocity, b, dt, dx, C)
-#     H_u2 = height * velocity * velocity
-#     dzeta_dx =  central_derivative(height, dx)
-#     third_term = C.g * height * dzeta_dx
-#     return (-velocity * dH_dt - central_derivative(H_u2, dx) - third_term - C.beta * velocity / height) / height
 
 
 def Hu_time_deriv(
@@ -125,7 +88,7 @@ def Hu_time_deriv(
         dx: float,
         C: Any
     ) -> np.ndarray:
-    """ 
+    """
     Function that generates values for the time derivative
     of the velocity for one iteration step over dt
     
@@ -139,16 +102,33 @@ def Hu_time_deriv(
     """                     
     H_u2 = height * velocity**2
     helper_var = H_u2 + (C.g / 2) * height**2
-    return -central_derivative(helper_var, dx) - C.beta * (velocity / height)
+    return -CD(helper_var, dx) - C.beta * (velocity / height)
 
+
+def init_Gaussian_wave(
+        midpoint: float,
+        std: float,
+        C: Any
+    ) -> np.array:
+    """ Inits an array with a Gaussian curvature
+
+    :param midpoint: midpoint
+    :param std: std
+    :param C: class (struct) with hyperparams
+    :return: array with Gaussian curvature
+    """ 
+    array = np.ndarray((C.n_x + 1), dtype = float)
+    for idx, _ in enumerate(array):
+        array[idx] = np.e**(-(0.5) * (idx - midpoint)**2 / (std)) \
+                     / (std * np.sqrt(2 * np.pi))
+    return array
+    
 
 def Task3_caller(
         L: int,
         nx: int,
         t_total: int,
         dt: int,
-        TimeSteppingMethod: str, 
-        DiffMethod: str = "CD"
     ) -> Tuple[np.array, np.array, np.array]:
     """ This routine calls the function that solves the heat equation
 
@@ -185,214 +165,116 @@ def Task3_caller(
                                 # init grids for H and Hu
     h_grid = np.zeros((nx + 1, C.n_t + 1), dtype = float)
     Hu_grid = np.zeros((nx + 1, C.n_t + 1), dtype = float)
-                                # for h, set first row to 1, and first column to 2
-    h_grid[:, 0] = 1.0
-    h_grid[0, :] = 2.0
-                                # init u grid with 1, and set first row to 3
-    u_grid = np.ones((nx + 1, C.n_t + 1))
-    u_grid[0, :] = 3.0
+                    
+                                # init a normally distributed grid
+    height = 5
+    h_grid[:, 0] = init_Gaussian_wave((C.n_x + 1) / 2, 20, C) + height
+
+                                # init u grid with 1, and set u to 0 on boundaries
+    u_grid = np.zeros((nx + 1, C.n_t + 1))
+    u_grid[0, :] = 0.0
+    u_grid[-1, :] = 0.0
                                 # fill Hu grid with product of h and u
     Hu_grid = u_grid * h_grid
                                 # b array is filled with constant value
-    b_arr = np.zeros(nx + 1)
-    b_arr[:] = C.b
+    b = np.full(nx + 1, C.b)
                                 # create time and space arrays
     time = np.linspace(0, t_total, C.n_t + 1)
     space = np.linspace(0, L, nx + 1)
 
-    #TODO! Implementeer realistische golven! 
-    #TODO! Check welke methode echt klopt, de u- of de Hu-methode!!!
-                                
-    if TimeSteppingMethod == "FE":
-        for idx in range(1, len(time)):
-            H_old = h_grid[:, idx - 1]
-            Hu_old = Hu_grid[:, idx - 1]
-            u_old = Hu_old / H_old
+                                # Iterate over time!
+    for idx in range(1, len(time)):
+                                # we fix the velocity at the boundaries
+        u = Hu_grid[:, idx - 1] / h_grid[:, idx - 1]
+        u[0] = 0.0
+        u[-1] = 0.0
 
-            h_grid[:, idx] = forward_euler(
-                H_old,
-                H_old, 
-                u_old,
-                b_arr,
-                C.dt,
-                C.dx,
-                H_time_deriv,
-                C
-            )
-            Hu_grid[:, idx] = forward_euler(
-                Hu_old,
-                H_old,
-                u_old,
-                b_arr,
-                C.dt,
-                C.dx,
-                Hu_time_deriv,
-                C
-            )
+        h_grid[:, idx] = RK4(
+            h_grid[:, idx - 1],
+            h_grid[:, idx - 1],
+            u, b, C.dt, C.dx, H_time_deriv, C
+        )
+        Hu_grid[:, idx] = RK4(
+            Hu_grid[:, idx - 1],
+            h_grid[:, idx - 1],
+            u, b, C.dt, C.dx, Hu_time_deriv, C
+        )
 
-    # elif TimeSteppingMethod == "LF":
-    #     for idx in range(1, len(time)):
-    #         if idx == 1:
-    #             h_grid[:, idx] = forward_euler(h_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, H_time_deriv, C)
-    #             Hu_grid[:, idx] = forward_euler(Hu_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, Hu_time_deriv, C)
-    #         else:
-    #             h_grid[:, idx] = leap_frog(h_grid[:,idx - 2], h_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, H_time_deriv, C)
-    #             Hu_grid[:, idx] = leap_frog(Hu_grid[:, idx - 2], Hu_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, Hu_time_deriv, C)
-
-    # elif TimeSteppingMethod == "AB":
-    #     for idx in range(1, len(time)):
-    #         if idx == 1:
-    #             h_grid[:, idx] = forward_euler(h_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, H_time_deriv, C)
-    #             Hu_grid[:, idx] = forward_euler(Hu_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, Hu_time_deriv, C)
-    #         else:
-    #             h_grid[:, idx] = adam_bashforth(h_grid[:, idx - 1], h_grid[:, idx - 2], 
-    #                                            Hu_grid[:, idx - 2], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, H_time_deriv, C)
-    #             Hu_grid[:, idx] = adam_bashforth(Hu_grid[:, idx - 1], h_grid[:, idx - 2], 
-    #                                            Hu_grid[:, idx - 2], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, Hu_time_deriv, C)
-
-    # elif TimeSteppingMethod == "CN":
-    #     h_grid[:] = 1
-    #     Hu_grid[:] = 1
-    #     # A, B = init_crank_nicolson(C)
-    #     # for idx in range(1, len(time)):
-    #     #     grid[:, idx] = crank_nicolson(grid[:, idx - 1], A, B, C)
-    #     # TODO! maak Crank nicolson methode voor deze -> A,B maken
-        
-    # elif TimeSteppingMethod == "RK4":
-    #     for idx in range(1, len(time)):
-    #             h_grid[:, idx] = runge_kutta_4(h_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, H_time_deriv, C)
-    #             Hu_grid[:, idx] = runge_kutta_4(Hu_grid[:, idx - 1], h_grid[:, idx - 1],
-    #                                            Hu_grid[:, idx - 1], b_arr,
-    #                                            C.dt, C.dx, Hu_time_deriv, C)
-    else:
-        raise ValueError("Invalid TimeSteppingMethod")
-    
-    u_grid_final = Hu_grid / h_grid
-
-    return time, space, (h_grid, u_grid_final)
+    return time, space, h_grid, Hu_grid / h_grid
 
 
 def main():
     C = PhysConstants()
-    C.L = 0.5
-    C.n_x = 20
-    C.t_total = 0.01
+    C.L = 10
+    C.n_x = 200
+    C.t_total = 1
     C.dt = 1e-5
 
-    time, space, FE_grid_tuple = Task3_caller(
+    time, space, h_grid, u_grid = Task3_caller(
         C.L,
         C.n_x,
         C.t_total,
-        C.dt,
-        "FE")
-    h_grid, u_grid_final = FE_grid_tuple
+        C.dt
+    )
 
-    indices = np.linspace(0, len(time) - 1, 5, dtype = int)
+    colours = ('#0A97B0', '#AE445A', '#2A3335', '#F29F58',
+               '#355F2E', '#AE445A', '#A27B5C', '#FFCFEF')
+    
     plt.figure(figsize = (10, 5))
-    
-    for idx in indices:
-        plt.plot(space, h_grid[:, idx], marker = 'o', label = f't = {time[idx]:.6f}s')
-    
-    plt.title('column height over position for different t (FE)', fontsize = 20)
-    plt.xlabel('position x (m)', fontsize = 20)
-    plt.ylabel('height H (m)', fontsize = 20)
-    plt.legend(fontsize = 20)
+
+    for t, col in zip(range(0, C.n_t, int(C.n_t / 5)), colours):
+        plt.plot(space, h_grid[:, t], marker = '.', color = col,
+                 label = f't = {time[t]:.4f}s')
+
+    plt.title(f'water distribution over time for RK4', fontsize = 20)
+    plt.xlabel('position x $(m)$', fontsize = 20)
+    plt.ylabel('height H $(m)$', fontsize = 20)
     plt.xticks(fontsize = 20)
     plt.yticks(fontsize = 20)
-    plt.grid(True)
-    plt.tight_layout()
+    plt.legend(fontsize = 20)
+    plt.grid()
     plt.show()
-    
-    # time, space, LF_grid_tuple = Task3_caller(
-    #     C.L,
-    #     C.n_x,
-    #     C.t_total,
-    #     C.dt,
-    #     "LF")
-    
-    # time, space, AB_grid_tuple = Task3_caller(
-    #     C.L,
-    #     C.n_x,
-    #     C.t_total,
-    #     C.dt,
-    #     "AB")
-    
-    # time, space, CN_grid_tuple = Task3_caller(
-    #     C.L,
-    #     C.n_x,
-    #     C.t_total,
-    #     C.dt,
-    #     "CN")
-    
-    # time, space, RK4_grid_tuple = Task3_caller(
-    #     C.L,
-    #     C.n_x,
-    #     C.t_total,
-    #     C.dt,
-    #     "RK4")
 
 
-    # height_grids = [FE_grid_tuple[0], LF_grid_tuple[0], AB_grid_tuple[0], 
-    #                 CN_grid_tuple[0], RK4_grid_tuple[0]]
-    # velocity_grids = [FE_grid_tuple[1], LF_grid_tuple[1], AB_grid_tuple[1], 
-    #                 CN_grid_tuple[1], RK4_grid_tuple[1]]
-    
-    # # Next, we plot our data in stationary graphs. We get 5 plots, one for each method's
-    # # comparison with the theoretical solution.
-    # d_h_results = {'Forward Euler' : FE_grid_tuple[0],
-    #              'Leap-frog' : LF_grid_tuple[0],
-    #              'Adams-Bashforth' : AB_grid_tuple[0],
-    #              'Crank-nicolson' : CN_grid_tuple[0],
-    #              'Runge-Kutta 4' : RK4_grid_tuple[0]
-    #              }
-    
-    # d_v_results = {'Forward Euler' : FE_grid_tuple[1],
-    #              'Leap-frog' : LF_grid_tuple[1],
-    #              'Adams-Bashforth' : AB_grid_tuple[1],
-    #              'Crank-nicolson' : CN_grid_tuple[1],
-    #              'Runge-Kutta 4' : RK4_grid_tuple[1]
-    #              }
-    # d_variables = {'Height': d_h_results, 'Velocity': d_v_results}
-    
-    # colours = ('#0A97B0', '#AE445A', '#2A3335', '#F29F58',
-    #            '#355F2E', '#AE445A', '#A27B5C', '#FFCFEF')
-    
-    # for name, variables in d_variables.items():
-    #     plt.figure()
-    #     plt.plot(1,1) # Dit is voor de duidelijkheid, later weghalen!
-    #     plt.show
-    #     for method in variables.keys():
-    #         plt.figure(figsize = (15, 4))
-    
-    #         for t, col in zip(range(0, C.n_t, int(C.n_t / 5)), colours):
-    #             plt.plot(space, variables[method][:, t], marker = '.', color = col,
-    #                      label = f't = {time[t]:.4f}s')
-    #         plt.title(f'{name} of fluid computed through {method}', fontsize = 20)
-    #         plt.xlabel('Position from end of basin $(m)$', fontsize = 20)
-    #         plt.ylabel('Height from bottom of basin $(m)$', fontsize = 20)
-    #         plt.xticks(fontsize = 20)
-    #         plt.yticks(fontsize = 20)
-    #         plt.legend(fontsize = 20, loc = 'upper right')
-    #         plt.grid()
-    #     plt.show()
+    # Wave animation:
+
+    fig, ax = plt.subplots()    
+                                # unpack Tuple with ',' for single element
+    line, = ax.plot([], [], lw = 2, label = "RK4", color = '#0A97B0')
+
+                                # set limits, labels, grid, legend, ticks
+    ax.set_title('1D Navier-Stokes wave simulation', fontsize = 20)
+    ax.set_xlabel("position x $(m)$", fontsize = 20)
+    ax.set_ylabel("height H $(m)$", fontsize = 20)
+    ax.set_xlim(space.min(), space.max())
+    ax.set_ylim(np.nanmin(h_grid), np.nanmax(h_grid))
+    ax.grid(True, which = 'both')
+    plt.xticks(fontsize = 20)
+    plt.yticks(fontsize = 20)
+    plt.legend(fontsize = 20, facecolor = '#F0F0F0')
+
+    def init_wave():
+        """" Create empty plot animation """
+        line.set_data([], [])
+        return (line, )
+
+    def update_wave(frame):
+        """ Update plot animation with new data """
+        line.set_data(space, h_grid[:, frame])
+        ax.set_title(f"t = {time[frame]:.4f} s", fontsize = 20)
+        return (line, )
+
+    frame_skip = int(max(1, len(time) // (C.n_t / 5)))
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.animation.FuncAnimation.html
+    ani = animation.FuncAnimation(fig,
+                                  update_wave,
+                                  frames = range(0, len(time), frame_skip),
+                                  init_func = init_wave,
+                                  interval = 1,
+                                #   blit = True,
+                                  repeat = True
+                                  )
+    plt.show()
 
 
 if __name__ == '__main__':
